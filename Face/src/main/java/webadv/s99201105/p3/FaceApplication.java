815fd168e172304320dfebb55972c0953ae9fed2
S-16202205 <weijialihai@qq.com>
2019-06-14 22:55:19 +0800

    物业管理系统房屋和业主部分

---
+++ Face/src/main/java/webadv/s99201105/p3/FaceApplication.java
@@ -0,0 +1,12 @@
+package webadv.s99201105.p3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaceApplication.class, args);
+	}
+}
---
+++ Face/src/test/java/webadv/s99201105/p3/FaceApplicationTests.java
@@ -0,0 +1,16 @@
+package webadv.s99201105.p3;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FaceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
