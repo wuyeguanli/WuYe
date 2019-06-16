83af19250a364340c88a345556bc1bdc93d38b73
S16202212 <lai314159@126.com>
2019-06-16 13:49:57 +0800

    我的项目模块

---
+++ p8/src/main/java/webadv/s16202212/p8/HomeController.java
@@ -0,0 +1,55 @@
+package webadv.s16202212.p8;
+
+import java.awt.List;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class HomeController {
+//	@GetMapping("/")
+//	public String index(Model model, String account, String password) {
+//		if ("abc".equals(account) && "123".equals(password))
+//			return "home";
+//		else
+//			return "index";
+//	}
+
+	@GetMapping("/login")
+	public String login(Model model) {
+		return "home";
+	}
+
+	@GetMapping("/wjAddHouse")
+	public String AddHouse(Model model) {
+		return "wjAddHouse";
+	}
+
+	@GetMapping("/wjSearchHouse")
+	public String search(Model model) {
+		return "wjSearchHouse";
+	}
+
+	@GetMapping("/wjAddUser")
+	public String wjAddUser(Model model) {
+		return "wjAddUser";
+	}
+	
+	@GetMapping("/wjSearchUser")
+	public String wjSearchUser(Model model) {
+		return "wjSearchUser";
+	}
+	@GetMapping("/lswaddNotice")
+	public String lswaddNotice(Model model) {
+		return "lswaddNotice";
+	}
+	@GetMapping("/lswdelectNotice")
+	public String lswdelectNotice(Model model) {
+		return "lswdelectNotice";
+	}
+	
+}
---
+++ p8/src/main/java/webadv/s16202212/p8/P8Application.java
@@ -0,0 +1,13 @@
+package webadv.s16202212.p8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P8Application.class, args);
+	}
+}
+
